af8ccada721178ad0dc8cf98548914aff4062d77
Lukasz Plac <Lukasz.Plac@woodward.com>
2017-09-22 08:56:44 +0200

    Next release. Styling for TodoList added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import style from './TodoList.css';
 
 const TodoList = (props) => {
-	let list = props.list.map(item => <li key={item.id} onClick = {() => props.remove(item.id)}>{item.text}</li>);
-	return <ul>{list}</ul>;
+	let list = props.list.map(item => <li 
+			key={item.id} 
+			onClick = {() => props.remove(item.id)}>{item.text}</li>);
+	return <ul className = {style.TodoList}>{list}</ul>;
 }
 
 export default TodoList;
